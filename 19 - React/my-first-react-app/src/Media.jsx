72c41bd36a421df3b270f1380c65de0631b7e72b
Kyla <kflmorcillos@gmail.com>
2024-10-03 14:31:39 +0800

    Fix syntax

--- 19 - React/my-first-react-app/src/Media.jsx
+++ 19 - React/my-first-react-app/src/Media.jsx
@@ -1,3 +1,3 @@
 export function ShikairoDaysGIF() {
-    return <img src="https://media1.tenor.com/m/3NbSlMJCjUsAAAAd/shikanoko-nokonoko-koshitantan-anime.gif"></img>
+    return <img src="https://media1.tenor.com/m/3NbSlMJCjUsAAAAd/shikanoko-nokonoko-koshitantan-anime.gif" />
 }
