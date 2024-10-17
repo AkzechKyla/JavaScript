c70d883c860e1342ecb3d90ff12975b37aa38790
Kyla <kflmorcillos@gmail.com>
2024-10-17 18:31:17 +0800

    Remove state from the child components

--- 19 - React/my-first-react-app/src/TestFunctions.jsx
+++ 19 - React/my-first-react-app/src/TestFunctions.jsx
@@ -1,9 +1,7 @@
 import { sculptureList } from './sculpture.js';
 import { useState } from 'react';
 
-function Panel({ title, children }) {
-  const [isActive, setIsActive] = useState(false);
-
+function Panel({ title, children, isActive}) {
   return (
     <section className="panel">
       <h3>{title}</h3>
@@ -19,17 +17,17 @@ function Panel({ title, children }) {
 }
 
 export function Accordion() {
-  return (
-    <>
-      <h2>Almaty, Kazakhstan</h2>
-      <Panel title="About">
-        With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
-      </Panel>
-      <Panel title="Etymology">
-        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
-      </Panel>
-    </>
-  );
+    return (
+        <>
+        <h2>Almaty, Kazakhstan</h2>
+        <Panel title="About" isActive={false}>
+            With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
+        </Panel>
+        <Panel title="Etymology" isActive={false}>
+            The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+        </Panel>
+        </>
+    );
 }
 
 
