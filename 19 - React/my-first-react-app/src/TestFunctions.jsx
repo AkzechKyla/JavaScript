9e6916046e292e6730581f2975d095f0fd17e7c1
Kyla <kflmorcillos@gmail.com>
2024-10-17 19:12:18 +0800

    Fix lint errors

--- 19 - React/my-first-react-app/src/TestFunctions.jsx
+++ 19 - React/my-first-react-app/src/TestFunctions.jsx
@@ -23,13 +23,13 @@ export function Accordion() {
         <>
         <h2>Almaty, Kazakhstan</h2>
         <Panel title="About" isActive={activeIndex === 0} onShow={() => setActiveIndex(0)}>
-            With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
+            With a population of about 2 million, Almaty is Kazakhstan&apos;s largest city. From 1929 to 1997, it was its capital city.
         </Panel>
         <Panel title="Etymology" isActive={activeIndex === 1} onShow={() => setActiveIndex(1)}>
-            The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+            The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for &quot;apple&quot; and is often translated as &quot;full of apples&quot;. In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
         </Panel>
         <Panel title="Testing 2" isActive={activeIndex === 2} onShow={() => setActiveIndex(2)}>
-            The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+            The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for &quot;apple&quot; and is often translated as &quot;full of apples&quot;. In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
         </Panel>
         </>
     );
@@ -99,10 +99,10 @@ export function Gallery() {
         }
     }
 
-    function handleMoreClick() {
-        // showMore = !showMore
-        setShowMore(!showMore);
-    }
+    // function handleMoreClick() {
+    //     // showMore = !showMore
+    //     setShowMore(!showMore);
+    // }
 
     let sculpture = sculptureList[index];
 
