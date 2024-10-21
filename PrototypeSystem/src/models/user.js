618a8f59e1e173f1da45f3c57439b6eac77159dc
Kyla <kflmorcillos@gmail.com>
2024-10-21 22:10:56 +0800

    Add sign in with Google popup

--- PrototypeSystem/src/models/user.js
+++ PrototypeSystem/src/models/user.js
@@ -1,6 +1,17 @@
+import {auth, provider} from './services/';
+import {signInWithPopup} from "firebase/auth";
+
 class User {
     constructor() {
         this.user = null;
+    }
+
+    async signInWithGoogle() {
+        const result = await signInWithPopup(auth, provider);
+        this.user = result.user;
+
+        console.log('User signed in:', this.user);
+        return this.user;
     }
 }
 
