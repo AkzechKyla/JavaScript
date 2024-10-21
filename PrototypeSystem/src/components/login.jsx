e709b69bdd07df2fae104453dfffe1189ddb4d53
Kyla <kflmorcillos@gmail.com>
2024-10-21 19:15:54 +0800

    Add sign in popup

--- PrototypeSystem/src/components/login.jsx
+++ PrototypeSystem/src/components/login.jsx
@@ -1,5 +1,16 @@
+import {auth, provider, signInWithPopup} from '../services/auth'
+
 export function LoginButton() {
+    const signInWithGoogle = async () => {
+        try {
+          const result = await signInWithPopup(auth, provider);
+          console.log(result.user);
+        } catch (error) {
+          console.error("Error signing in with Google: ", error);
+        }
+    };
+
     return(
-        <button>Login with Google</button>
+        <button onClick={signInWithGoogle}>Login with Google</button>
     )
 }
