7a4bbfa216e652c645276505a06a0ae0283980df
Kyla <kflmorcillos@gmail.com>
2024-10-21 10:37:49 +0800

    Add sign in popup onclick

--- PrototypeProject/src/components/login.tsx
+++ PrototypeProject/src/components/login.tsx
@@ -1,5 +1,16 @@
+import { auth, provider, signInWithPopup} from '../services/firebase';
+
 export function LoginButton() {
+    const signInWithGoogle = async () => {
+        try {
+          const result = await signInWithPopup(auth, provider);
+          console.log(result.user); // User info is available here
+        } catch (error) {
+          console.error("Error signing in with Google: ", error);
+        }
+    };
+
     return (
-        <button onClick={}>Login with Google</button>
+        <button onClick={signInWithGoogle}>Login with Google</button>
     )
 }
