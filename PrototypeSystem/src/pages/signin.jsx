22ec6c74242a1a675d255b54d2a248e1acaad7c7
Kyla <kflmorcillos@gmail.com>
2024-10-23 00:01:10 +0800

    Add Sign In page

---
+++ PrototypeSystem/src/pages/signin.jsx
@@ -0,0 +1,11 @@
+import {LoginButton} from '../components/login'
+
+export function SignIn() {
+    return(
+        <div>
+            <p>Welcome</p>
+            <p>Please log in or sign in below</p>
+            <LoginButton/>
+        </div>
+    )
+}
