e92353c397f3201c413df7e8b2e2b181d620ab1a
Kyla <kflmorcillos@gmail.com>
2024-10-23 10:28:21 +0800

    Move login button inside sign in page

--- PrototypeSystem/src/pages/signin.jsx
+++ PrototypeSystem/src/pages/signin.jsx
@@ -1,11 +1,24 @@
-import {LoginButton} from '../components/login'
+import User from '../models/user';
+import { useState } from 'react';
 
 export function SignIn() {
+    const [user, setUser] = useState(null);
+    const userInstance = new User();
+
+    const signInWithGoogle = async () => {
+        userInstance.signInWithGoogle();
+        setUser(userInstance.getUser());
+
+        if (userInstance.getUserRole() == 'admin') {
+            console.log('this is an admin');
+        }
+    };
+
     return(
         <div>
             <p>Welcome</p>
             <p>Please log in or sign in below</p>
-            <LoginButton/>
+            <button onClick={signInWithGoogle}>Login with Google</button>
         </div>
     )
 }
