9e2c32d3c60a936743801e20d58af6b3ccb6ad47
Kyla <kflmorcillos@gmail.com>
2024-10-23 14:11:56 +0800

    Add portal page

---
+++ PrototypeSystem/src/pages/portal.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export function Portal() {
+    return(
+        <div>
+            <p>Welcome</p>
+            <p>Choose your destination</p>
+            <button>Student</button>
+            <button>Admin</button>
+        </div>
+    )
+}
