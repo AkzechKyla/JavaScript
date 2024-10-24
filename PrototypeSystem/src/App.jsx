0b2f970dcadfc4d1cb5e932343ba1f9348fc64be
Kyla <kflmorcillos@gmail.com>
2024-10-24 23:47:10 +0800

    Add home page

--- PrototypeSystem/src/App.jsx
+++ PrototypeSystem/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {SignIn} from './pages/signin'
 import {Portal} from './pages/portal'
+import {Home} from './pages/home';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<SignIn/>} />
         <Route path='/select-portal' element={<Portal/>} />
+        <Route path='/home' element={<Home/>} />
       </Routes>
     </Router>
   )
---
+++ PrototypeSystem/src/pages/home.jsx
@@ -0,0 +1,9 @@
+export function Home() {
+    return(
+        <div>
+            <p>CCIS Concern Hub</p>
+            <p>A Transparent, Efficient, and Streamlined Concern Management System</p>
+            <button>Submit Concern</button>
+        </div>
+    )
+}
