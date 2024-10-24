cdfc81c764204027ce531b88a4bc6b8a36acaacc
Kyla <kflmorcillos@gmail.com>
2024-10-25 00:53:21 +0800

    Add register page

--- PrototypeSystem/src/App.jsx
+++ PrototypeSystem/src/App.jsx
@@ -1,7 +1,8 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import LandingPage from './pages/landingPage';
-import {SignIn} from './pages/signin'
+import SignIn from './pages/signin'
 import Portal from './pages/portal'
+import Register from './pages/register'
 
 
 function App() {
@@ -9,9 +10,9 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<LandingPage/>} />
-        {/* <Route path='/' element={<SignIn/>} /> */}
+        <Route path='/sign-in' element={<SignIn/>} />
         <Route path='/select-portal' element={<Portal/>} />
-        <Route path='/home' element={<Home/>} />
+        <Route path='/register' element={<Register/>} />
       </Routes>
     </Router>
   )
---
+++ PrototypeSystem/src/pages/register.jsx
@@ -0,0 +1,11 @@
+function Register() {
+    return(
+        <div>
+            <h1>Create An Account</h1>
+            <h2>Fill up the necessary details below</h2>
+            <button>Submit</button>
+        </div>
+    )
+}
+
+export default Register;
