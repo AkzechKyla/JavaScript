12f2327a24ed04e61c54e7415669071a261d4d9c
Kyla <kflmorcillos@gmail.com>
2024-10-26 17:57:16 +0800

    Use Microsoft authentication

--- PrototypeSystem/src/auth/auth.js
+++ PrototypeSystem/src/auth/auth.js
@@ -1,8 +1,12 @@
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import {getAuth, OAuthProvider} from "firebase/auth";
 import {app} from '../services/firebase';
 
 const auth = getAuth(app);
 auth.languageCode = 'en';
-const provider = new GoogleAuthProvider()
+
+const provider = new OAuthProvider('microsoft.com')
+provider.setCustomParameters({
+    tenant: "iskolarngbayan.pup.edu.ph" // Restrict to PUP domain if applicable
+});
 
 export {auth, provider};
