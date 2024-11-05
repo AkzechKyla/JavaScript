18c8eaa7a2f0195db3fbf89840997325559a64da
Kyla <kflmorcillos@gmail.com>
2024-11-05 12:18:32 +0800

    Add ChatMessage component

--- ChatApp/src/App.jsx
+++ ChatApp/src/App.jsx
@@ -67,4 +67,16 @@ function ChatRoom() {
   </>;
 }
 
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent': 'received';
+
+  return(
+    <div className={`message ${messageClass}`}>
+      <p>{text}</p>
+    </div>
+  )
+}
+
 export default App
