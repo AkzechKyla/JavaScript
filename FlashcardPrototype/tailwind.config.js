9f3b14a0b36b7e50df2f6baa0ce18042eb3dc45b
Kyla <kflmorcillos@gmail.com>
2024-11-08 11:26:55 +0800

    Add default styles

--- FlashcardPrototype/tailwind.config.js
+++ FlashcardPrototype/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,5 +8,13 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl') },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+      })
+    })
+  ],
 }
