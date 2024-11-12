eecbff81a54b23adb9e4ebcb4a6a132ec1fe4bb5
Kyla <kflmorcillos@gmail.com>
2024-11-12 20:28:35 +0800

    Add sample data

---
+++ FlashcardPrototype/src/data/questions.ts
@@ -0,0 +1,12 @@
+interface Question {
+  image: string;
+  answer: string;
+}
+
+const questions: Question[] = [
+  { image: 'https://lh6.googleusercontent.com/UIVWucNCaO5i80gV4avqtMtpI8OREza1FhcmV4uULqm8PxPDP-HB3n0OejNGrj0Bojn6fMwBgNTULFhHZ7ulBMtPBLUzJqrGmWm0ypcJtVTj-W8UFEGvEnWtigVbojtU8Q=w740', answer: 'cassiopeia' },
+  { image: 'https://lh6.googleusercontent.com/Cn3yFiz9HnZTLB7WBVHATNQ2YMrSThU7rxY5JnFKkaUQj1Ao0e0n8MbzuX_ubUMN_sj1yXxw67O8jCeptwdE6Y9Ulzu-SQJbqQigtM1hFPdO8cdBfchOsvmHekijZcnDAg=w740', answer: 'rick' },
+  { image: 'https://example.com/image3.jpg', answer: 'test' },
+];
+
+export default questions;
