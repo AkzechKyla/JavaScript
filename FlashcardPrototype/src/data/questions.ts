de67ded91256f7125ca598abaacf00738bcc5ccb
Kyla <kflmorcillos@gmail.com>
2024-11-15 20:40:24 +0800

    Add test data

--- FlashcardPrototype/src/data/questions.ts
+++ FlashcardPrototype/src/data/questions.ts
@@ -6,7 +6,7 @@ interface Question {
 const questions: Question[] = [
   { image: 'https://lh6.googleusercontent.com/UIVWucNCaO5i80gV4avqtMtpI8OREza1FhcmV4uULqm8PxPDP-HB3n0OejNGrj0Bojn6fMwBgNTULFhHZ7ulBMtPBLUzJqrGmWm0ypcJtVTj-W8UFEGvEnWtigVbojtU8Q=w740', answer: 'cassiopeia' },
   { image: 'https://lh6.googleusercontent.com/Cn3yFiz9HnZTLB7WBVHATNQ2YMrSThU7rxY5JnFKkaUQj1Ao0e0n8MbzuX_ubUMN_sj1yXxw67O8jCeptwdE6Y9Ulzu-SQJbqQigtM1hFPdO8cdBfchOsvmHekijZcnDAg=w740', answer: 'rick' },
-  { image: 'https://example.com/image3.jpg', answer: 'test' },
+  { image: 'https://i.pinimg.com/736x/67/0e/1f/670e1fc607d825c1a783f4308043be28.jpg', answer: 'test' },
 ];
 
 export default questions;
