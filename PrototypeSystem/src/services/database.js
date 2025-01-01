1199c3aa110472fd109ab8b6ae91d40aef569b2b
Kyla <kflmorcillos@gmail.com>
2025-01-01 23:01:25 +0800

    Add Database class

---
+++ PrototypeSystem/src/services/database.js
@@ -0,0 +1,45 @@
+import { collection, query, where, doc, getDocs, getDoc, setDoc } from "firebase/firestore";
+import { firestore } from "./firebase";
+import User from "../models/user";
+import Concern from "../models/concern";
+
+export default class Database {
+    static async getUser(uid) {
+        const docRef = doc(firestore, "users", uid);
+        const docSnap = await getDoc(docRef);
+        return docSnap.exists() ? new User(docSnap.data()) : null;
+    }
+
+    static async setUserData(uid, userData) {
+        const docRef = doc(firestore, "users", uid);
+        await setDoc(docRef, userData.toPlainObject());
+        return new User(userData);
+    }
+
+    static async getConcern(concernId) {
+        const docRef = doc(firestore, "concerns", concernId);
+        const docSnap = await getDoc(docRef);
+        return docSnap.exists() ? new Concern(docSnap.data()) : null;
+    }
+
+    static async getUserConcerns(uid) {
+        const q = query(collection(firestore, "concerns"), where("creatorUid", "==", uid));
+        const querySnap = await getDocs(q);
+        const concerns = querySnap.docs.map(doc => {
+            return new Concern(doc.data());
+        });
+        return concerns;
+    }
+
+    static getNewConcernId() {
+        const concernsCollectionRef = collection(firestore, "concerns");
+        const docRef = doc(concernsCollectionRef);
+        return docRef.id;
+    }
+
+    static async setConcern(concernData, concernId) {
+        const concernsCollectionRef = collection(firestore, "concerns");
+        const docRef = doc(concernsCollectionRef);
+        await setDoc(docRef, { ...concernData, id: concernId });
+    }
+}
