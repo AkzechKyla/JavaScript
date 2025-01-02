1d5d8295408690390bbb986fd630d293aa2ce4ac
Kyla <kflmorcillos@gmail.com>
2025-01-02 23:05:16 +0800

    Refactor entry point of app

--- PrototypeSystem/src/App.jsx
+++ PrototypeSystem/src/App.jsx
@@ -1,24 +1,97 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import { useState } from 'react';
-import SignIn from './pages/signin'
-import Portal from './pages/portal'
-import Register from './pages/register'
-import AdminDashboard from './pages/adminDashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { SignIn } from './pages/signin';
+import { Portal } from './pages/portal';
+import { SubmitConcern } from './pages/submitConcern';
+import { MyConcerns } from "./pages/myConcerns";
+import { AdminDashboard } from "./pages/adminDashboard";
+import { ViewConcern } from './pages/viewConcern';
+import RouteElement from './components/route';
+import ClientUser from './auth/clientUser';
 
+export default function App() {
+  /** @type {[undefined | null | ClientUser, any]} */
+  const [clientUser, _setClientUser] = useState(undefined);
+  const [userData, setUserData] = useState(null);
 
-function App() {
-  const [authenticatedUser, setAuthenticatedUser] = useState(null);
+  useEffect(() => {
+    ClientUser.onAuthStateChanged((firebaseUser) => {
+      if (firebaseUser === null) {
+        _setClientUser(null);
+      } else {
+        _setClientUser(new ClientUser(firebaseUser));
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (clientUser) {
+        setUserData(await clientUser.getUserFromDatabase());
+      } else {
+        setUserData(null);
+      }
+    })();
+  }, [clientUser]);
+
+  const routeMap = {
+    "/signin": {
+      "element": <SignIn clientUser={clientUser} />,
+      "headerProps": {
+        "isSolid": false,
+      }
+    },
+    "/portal": {
+      "element": <Portal />,
+      "headerProps": {
+        "isSolid": false,
+      }
+    },
+    "/submit-concern": {
+      "element": <SubmitConcern userData={userData} />,
+      "headerProps": {
+        "isSolid": true,
+      }
+    },
+    "/my-concerns": {
+      "element": <MyConcerns userData={userData} />,
+      "headerProps": {
+        "isSolid": true,
+      }
+    },
+    "/admin-dashboard": {
+      "element": <AdminDashboard userData={userData} />,
+      "headerProps": {
+        "isSolid": true,
+      }
+    },
+    "/view-concern/:concernId": {
+      "element": <ViewConcern userData={userData} />,
+      "headerProps": {
+        "isSolid": true,
+      }
+    },
+  }
+
+  routeMap["/"] = routeMap["/signin"]; // The root path is just the signin page
+  routeMap["*"] = routeMap["/signin"]; // Fallback to signin page if the user went to an invalid path
 
   return (
     <Router>
       <Routes>
-        <Route path='/sign-in' element={<SignIn setAuthenticatedUser={setAuthenticatedUser} />} />
-        <Route path='/select-portal' element={<Portal/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        {
+          Object.entries(routeMap).map(([path, value]) => {
+            return <Route key={path} path={path} element={
+              <RouteElement
+                element={value["element"]}
+                headerProps={value["headerProps"]}
+                clientUser={clientUser}
+                userData={userData}
+              />
+            } />;
+          })
+        }
       </Routes>
     </Router>
-  )
+  );
 }
-
-export default App
