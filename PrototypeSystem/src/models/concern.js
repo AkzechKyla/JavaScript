f656873d40374d3825b6a23a8c061ec1161653d1
Kyla <kflmorcillos@gmail.com>
2025-01-03 23:48:32 +0800

    Add Concern methods

--- PrototypeSystem/src/models/concern.js
+++ PrototypeSystem/src/models/concern.js
@@ -1,18 +1,83 @@
 import { Timestamp } from "firebase/firestore";
+import Database from "../services/database";
+import Storage from '../services/storage';
 
 export default class Concern {
-    constructor({ attachmentLinks, category, creatorDisplayName, creatorUid, dateSubmitted, description, id, isResolved, isSpam, issueType, status, subject }) {
+    constructor({ attachmentLinks, category, creatorUid, dateSubmitted, description, uid, isResolved = false, isSpam = false, issueType, status = 'Open', subject }) {
         this.attachmentLinks = attachmentLinks;
         this.category = category;
-        this.creatorDisplayName = creatorDisplayName;
         this.creatorUid = creatorUid;
         this.dateSubmitted = dateSubmitted instanceof Timestamp ? dateSubmitted.toDate() : new Date(dateSubmitted);
         this.description = description;
-        this.id = id;
+        this.uid = uid || Database.generateConcernUid();
         this.isResolved = isResolved;
         this.isSpam = isSpam;
         this.issueType = issueType;
         this.status = status;
         this.subject = subject;
+    }
+
+    async saveToDatabase() {
+        await Database.setConcern(this);
+    }
+
+    async fetchCreator(userData) {
+        if (userData?.uid === this.creatorUid) {
+            return userData;
+        }
+
+        try {
+            return await Database.getUser(this.creatorUid);
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+            return null;
+        }
+    }
+
+    toJSON() {
+        return {
+            attachmentLinks: this.attachmentLinks,
+            category: this.category,
+            creatorUid: this.creatorUid,
+            dateSubmitted: this.dateSubmitted,
+            description: this.description,
+            uid: this.uid,
+            isResolved: this.isResolved,
+            isSpam: this.isSpam,
+            issueType: this.issueType,
+            status: this.status,
+            subject: this.subject,
+        };
+    }
+
+    async uploadAttachments(files) {
+        const promises = [];
+
+        for (const file of files) {
+            const p = Storage.uploadFile(file, `concerns/${this.uid}/${file.name}`);
+            promises.push(p);
+        }
+
+        await Promise.all(promises);
+    }
+
+    async fetchAttachments(path) {
+        return await Storage.retrieveFiles(path);
+    }
+
+    async setAttachmentLinks(files, path) {
+        const promises = [];
+
+        for (const file of files.items) {
+            const url = await Storage.getDownloadURL(file, `${path}/${file.name}`);
+            promises.push(url);
+        }
+
+        this.attachmentLinks = await Promise.all(promises);
+        console.log(this.attachmentLinks);
+    }
+
+    getAttachmentLinks() {
+        return this.attachmentLinks;
     }
 }
