f4886c79baf8843f3cd2b837aca16010963ea447
Kyla <kflmorcillos@gmail.com>
2025-01-06 22:33:15 +0800

    Paste adminDashboard code

--- PrototypeSystem/src/pages/adminDashboard.jsx
+++ PrototypeSystem/src/pages/adminDashboard.jsx
@@ -1,6 +1,53 @@
-function AdminDashboard() {
+import { useEffect, useState, useRef, useCallback } from 'react';
+import Footer from '../components/footer';
+import ConcernList from '../components/concernList';
+import DashboardStats from '../components/dashboardStats';
+import Database, { Pagination } from '../services/database';
+import LoadingSpinner from '../components/loading';
+
+export function AdminDashboard({ userData }) {
+    const [concerns, setConcerns] = useState(undefined);
+    const pagination = useRef(new Pagination(5));
+
+    const fetchCategoryConcerns = useCallback(async () => {
+        if (userData) {
+            const categoryConcerns = await Database.getCategoryConcerns(userData.assignedCategories, pagination.current)
+
+            if (concerns === undefined) {
+                setConcerns(categoryConcerns);
+            } else {
+                setConcerns([...concerns, ...categoryConcerns]);
+            }
+        }
+    }, [userData, concerns]);
+
+    useEffect(() => {
+        fetchCategoryConcerns();
+    }, [fetchCategoryConcerns]);
+
+    // Dummy metrics data
+    const metricsData = {
+        open: 5,
+        inProgress: 3,
+        onHold: 2,
+        resolved: 10,
+        unresolved: 1,
+        totalConcerns: 21,
+    };
+
     return (
-        <div>Admin Dashboard</div>
+        <div>
+            <main className="container mx-auto p-4">
+                <h2 className="text-2xl font-semibold mt-2 text-blue-400">Concern Overview</h2>
+                <DashboardStats metrics={metricsData} />
+                <h2 className="text-xl font-semibold mt-6 text-blue-400">Manage Concerns</h2>
+                {
+                    concerns === undefined ? <LoadingSpinner /> :
+                    <ConcernList concerns={concerns} fetchUserConcerns={fetchCategoryConcerns} />
+                }
+            </main>
+            <Footer />
+        </div>
     );
 }
 
