8b0009cd446a46e1a9c0523028d08514354b87b9
Kyla <kflmorcillos@gmail.com>
2025-01-21 12:34:00 +0100

    Update User methods

--- PrototypeSystem/src/models/user.js
+++ PrototypeSystem/src/models/user.js
@@ -1,27 +1,15 @@
 import Database from "../services/database";
 
 export default class User {
-    static async findByUid(uid) {
-        const userData = await Database.getUserData(uid);
-        return userData ? new User(userData) : null;
-    }
-
-    static async new(uid, displayName) {
-        const userData = {
-            uid: uid,
-            displayName: displayName,
-            roles: ["student"],
-            assignedCategories: null,
-        };
-        await Database.setUserData(uid, userData);
-        return new User(userData);
-    }
-
     constructor({ uid, displayName, roles, assignedCategories = null }) {
         this.uid = uid;
         this.displayName = displayName;
         this.roles = roles;
         this.assignedCategories = assignedCategories;
+    }
+
+    async saveToDatabase() {
+        await Database.setUser(this);
     }
 
     getAvatarUrl() {
@@ -34,5 +22,14 @@ export default class User {
 
     isAdmin() {
         return this.roles.includes('admin');
+    }
+
+    toJSON() {
+        return {
+            uid: this.uid,
+            displayName: this.displayName,
+            roles: this.roles,
+            assignedCategories: this.assignedCategories,
+        };
     }
 }
