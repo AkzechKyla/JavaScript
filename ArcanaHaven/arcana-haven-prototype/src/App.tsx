f0b5e5b9b3ae2a4210ef142bb479037a654351e3
Kyla <kflmorcillos@gmail.com>
2025-03-21 23:02:03 +0800

    Implement navigation menu

--- ArcanaHaven/arcana-haven-prototype/src/App.tsx
+++ ArcanaHaven/arcana-haven-prototype/src/App.tsx
@@ -1,7 +1,29 @@
 function App() {
-  return <>
+  return <div className="flex flex-col justify-center text-center">
       <p className="text-3xl font-bold underline">Arcana Haven Prototype</p>
-  </>
+      <nav>
+        <p className="text-xl font-bold">Personal</p>
+        <ul>
+          <li>Profile</li>
+          <li>Reading Collection</li>
+          <li>Sign Out</li>
+        </ul>
+        <p className="text-xl font-bold">Tarot Readings</p>
+        <ul>
+          <li>General Tarot</li>
+          <li>Ask a Question</li>
+          <li>Yes/No Tarot</li>
+        </ul>
+        <p className="text-xl font-bold">Card Meanings</p>
+        <ul>
+          <li>Major Arcana</li>
+          <li>Wands</li>
+          <li>Cups</li>
+          <li>Swords</li>
+          <li>Pentacles</li>
+        </ul>
+      </nav>
+  </div>
 }
 
 export default App
