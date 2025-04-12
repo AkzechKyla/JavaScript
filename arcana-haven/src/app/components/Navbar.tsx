3a9cbc135196289a139e79c53212bb3d9e52a124
Kyla <kflmorcillos@gmail.com>
2025-04-12 23:13:19 +0800

    Update font size

--- arcana-haven/src/app/components/Navbar.tsx
+++ arcana-haven/src/app/components/Navbar.tsx
@@ -3,24 +3,24 @@ import Link from "next/link";
 export default function Navbar() {
   return <>
         <nav>
-            <Link href="/" className="text-xl font-bold">Personal</Link>
+            <Link href="/" className="text-2xl font-bold">Personal</Link>
             <ul>
-                <Link href="/collection"><li>Reading Collection</li></Link>
-                <Link href="/"><li>Sign Out</li></Link>
+                <Link href="/collection"><li className="text-lg">Reading Collection</li></Link>
+                <Link href="/"><li className="text-lg">Sign Out</li></Link>
             </ul>
-            <Link href="/tarot" className="text-xl font-bold">Tarot Reading</Link>
+            <Link href="/tarot" className="text-2xl font-bold">Tarot Reading</Link>
             <ul>
-                <Link href="/tarot/general"><li>General Tarot</li></Link>
-                <Link href="/tarot/ask-question"><li>Ask a Question</li></Link>
-                <Link href="/tarot/yes-no"><li>Yes/No Tarot</li></Link>
+                <Link href="/tarot/general"><li className="text-lg">General Tarot</li></Link>
+                <Link href="/tarot/ask-question"><li className="text-lg">Ask a Question</li></Link>
+                <Link href="/tarot/yes-no"><li className="text-lg">Yes/No Tarot</li></Link>
             </ul>
-            <Link href="/cards" className="text-xl font-bold">Card Meanings</Link>
+            <Link href="/cards" className="text-2xl font-bold">Card Meanings</Link>
             <ul>
-                <Link href="/cards/major"><li>Major Arcana</li></Link>
-                <Link href="/cards/wands" ><li>Wands</li></Link>
-                <Link href="/cards/cups"><li>Cups</li></Link>
-                <Link href="/cards/swords"><li>Swords</li></Link>
-                <Link href="/cards/pentacles"><li>Pentacles</li></Link>
+                <Link href="/cards/major"><li className="text-lg">Major Arcana</li></Link>
+                <Link href="/cards/wands" ><li className="text-lg">Wands</li></Link>
+                <Link href="/cards/cups"><li className="text-lg">Cups</li></Link>
+                <Link href="/cards/swords"><li className="text-lg">Swords</li></Link>
+                <Link href="/cards/pentacles"><li className="text-lg">Pentacles</li></Link>
             </ul>
         </nav>
     </>
